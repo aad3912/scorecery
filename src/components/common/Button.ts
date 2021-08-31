dbfcfbae5eac1574b51092697970c7709107fe2e
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-08-31 13:16:14 +0100

    Add border for Button

--- src/components/common/Button.ts
+++ src/components/common/Button.ts
@@ -7,23 +7,27 @@ interface ButtonPropsT {
   color: string;
   borderRadius: numInPx | numInPct;
   largeFont?: boolean;
+  border?: boolean;
 }
 
 const Button = styled.button`
-  width: ${(props: ButtonPropsT) => (props.width ? props.width : "auto")};
+  min-width: max-content;
   height: ${(props: ButtonPropsT) => props.height};
   background-color: ${(props: ButtonPropsT) => props.bgColor};
   color: ${(props: ButtonPropsT) => props.color};
-  border: 1px solid ${(props: ButtonPropsT) => props.color};
+  border: 3px solid
+    ${(props: ButtonPropsT) => (props.border ? props.color : props.bgColor)};
   border-radius: ${(props) => props.borderRadius};
   text-decoration: none;
   font-size: ${(props: ButtonPropsT) => (props.largeFont ? "1.5rem" : "1rem")};
   padding: 7px 14px 7px 14px;
   cursor: pointer;
   transition: 0.2s all ease-in;
+  font-weight: bold;
 
   &:hover {
-    border: 1px solid ${(props: ButtonPropsT) => props.bgColor};
+    border: 3px solid
+      ${(props: ButtonPropsT) => (props.border ? props.bgColor : props.color)};
     color: ${(props: ButtonPropsT) => props.bgColor};
     background-color: ${(props: ButtonPropsT) => props.color};
   }
