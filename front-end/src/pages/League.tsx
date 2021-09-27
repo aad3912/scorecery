f2496da02a57dc4c637e7b43c797e9d5c8572237
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-09-27 14:04:05 +0100

    Made `/leagues` a private route

---
+++ front-end/src/pages/League.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Navbar from "../components/common/navbar/Navbar";
+import Footer from "../components/common/footer/Footer";
+import LeagueBox from "../components/leagues/LeagueBox";
+import { getFromApi } from "../components/common/constants";
+
+const getLeagues = () => {
+  const params: LeagueParamsT = {
+    current: "true",
+    code: "GB",
+  };
+
+  getFromApi("/leagues", params);
+};
+
+const League = () => {
+  return (
+    <>
+      <Navbar />
+      <LeagueBox />
+      <Footer />
+    </>
+  );
+};
+
+export default League;
