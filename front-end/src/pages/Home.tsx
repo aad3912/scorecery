d636410c363c48850181f64bfd511b27af4a181f
Dalal, Aayush <aayush.dalal19@imperial.ac.uk>
2021-10-02 14:55:48 +0100

    Refactored select component to be more generic.

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useContext, useEffect, useState } from "react";
 import Footer from "../components/common/footer/Footer";
 import Navbar from "../components/common/navbar/Navbar";
 import LeaguesSelector from "../components/widgets/leagues-select/LeaguesSelector";
@@ -13,31 +13,62 @@ import {
   HomeForm,
   HomeH2,
 } from "../components/home/HomeElements";
-import { config, useChain, useSpring, useSpringRef } from "@react-spring/core";
+import { getAuthHeader, getFromApi } from "../components/common/constants";
+// import { config, useChain, useSpring, useSpringRef } from "@react-spring/core";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const dummyLeagues: SelectOption[] = [
+  { label: "Premier League", value: 39 },
+  { label: "Ligue 1", value: 61 },
+  { label: "Bundesliga 1", value: 78 },
+  { label: "Primera Division", value: 140 },
+];
 
 const Home = ({ history }: RouterProps) => {
-  const [selectedLeagues, setSelectedLeagues] = useState<SelectOption[]>([]);
   const { userState, userDispatch } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [addOptionsL, setAddOptionsL] = useState<SelectOption[]>([]);
+  const [toBeAddedL, setToBeAddedL] = useState<SelectOption[]>([]);
 
   const emptyError = () => setTimeout(() => setError(""), 5000);
 
+  useEffect(() => {
+    async function getLeagues() {
+      const params: LeagueParamsT = { current: "true" };
+      const result = (await getFromApi(
+        "/leagues",
+        params
+      )) as LeaguesResponseT[];
+      const leagueSet = new Set(userState.leagues.map((league) => league._id));
+      setAddOptionsL(
+        result
+          .filter(
+            (match) =>
+              !leagueSet.has(match.league.id) && match.league.type === "League"
+          )
+          .map((match) => {
+            return {
+              label: `${match.league.name} [${match.country.name}]`,
+              value: match.league.id,
+            };
+          })
+      );
+    }
+    getLeagues();
+  }, [userState.leagues]);
+
   const addLeagues = async () => {
-    const mappedToLeagueDataT: LeagueDataT[] = selectedLeagues.map((league) => {
-      return { _id: league.value, name: league.label.split(/ \[/)[0] };
-    });
     try {
+      const mappedToLeagueData = toBeAddedL.map((match) => {
+        return { _id: match.value, name: match.label.split(/ \[/)[0] };
+      });
       await axios.post(
         "/api/leagues/add",
-        { leagues: mappedToLeagueDataT },
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-          },
-        }
+        { leagues: mappedToLeagueData },
+        { headers: { Authorization: getAuthHeader() } }
       );
-      userDispatch({ type: "ADD_LEAGUES", payload: mappedToLeagueDataT });
+      userDispatch({ type: "ADD_LEAGUES", payload: mappedToLeagueData });
       setLoading(false);
       history.push("/leagues");
     } catch (e) {
@@ -59,15 +90,15 @@ const Home = ({ history }: RouterProps) => {
     }
   };
 
-  const welcomeTextRef = useSpringRef();
-  const x = useSpring({
-    ref: welcomeTextRef,
-    config: { duration: 1500 },
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-  });
+  // const welcomeTextRef = useSpringRef();
+  // const x = useSpring({
+  //   ref: welcomeTextRef,
+  //   config: { duration: 1500 },
+  //   from: { opacity: 0 },
+  //   to: { opacity: 1 },
+  // });
 
-  useChain([welcomeTextRef], [0]);
+  // useChain([welcomeTextRef], [0]);
   // , show ? [0, 0.25, 0.75, 1.25] : [0, 0.5, 1, 1.5]);
 
   return (
@@ -75,13 +106,11 @@ const Home = ({ history }: RouterProps) => {
       <Navbar />
       <HomeContainer>
         <HomeWrapper>
-          <HomeWelcomeText style={x}>
-            Welcome, {userState.username}
-          </HomeWelcomeText>
+          <HomeWelcomeText>Welcome, {userState.username}</HomeWelcomeText>
           <HomeForm onSubmit={(e) => onSubmitForm(e, addLeagues)}>
             <FormError show={error}>{error}</FormError>
             <HomeH2>Add Leagues</HomeH2>
-            <LeaguesSelector setSelectedLeagues={setSelectedLeagues} />
+            <LeaguesSelector options={addOptionsL} setter={setToBeAddedL} />
             <FormButton type="submit">
               {loading ? "Adding Leagues..." : "Add"}
             </FormButton>
